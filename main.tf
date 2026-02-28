cb31f70b3271fbce53bcda00bb36722dc756e33d
shvkmr536 <shvkmr536@gmail.com>
2026-02-28 18:02:32 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "demo" {
   ami           = "ami-0030e4319cbf4dbf2" # Amazon Linux 2 (us-east-1)
   instance_type = var.instance_type
-
   tags = {
     Name        = "github-oidc-ec2"
     Environment = "Production"
