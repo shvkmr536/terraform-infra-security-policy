2c0e8780bb5be1439a07bab26209cd1fd5387d0d
shvkmr536 <shvkmr536@gmail.com>
2026-03-04 18:25:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "demo" {
+  ami           = "ami-0030e4319cbf4dbf2" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  #tags = {
+  # Name        = "github-oidc-ec2"
+  #  Environment = "Production"
+  #}
+#}
