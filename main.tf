e39fbf08058f4066c0a1cab3174b79afa861abc7
shvkmr536 <shvkmr536@gmail.com>
2026-03-04 18:42:27 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "demo" {
-  ami           = "ami-0030e4319cbf4dbf2" # Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
-
-  #tags = {
-  # Name        = "github-oidc-ec2"
-  #  Environment = "Production"
-  #}
-}
